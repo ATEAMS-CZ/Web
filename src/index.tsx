0c42cf965a14ca3f37d56cc77302b692e318855e
Vladimír Urík <gggedrvideos@gmail.com>
2022-11-22 20:37:36 +0100

    Add smooth scroll

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
 
 
 const container = document.getElementById('root');
