e7697d550c9c175ace2373c7c01e9bdb77f7fba0
Vladimír Urík <gggedrvideos@gmail.com>
2022-11-24 13:14:26 +0100

    Edit Add Server Modal

--- src/components/modals/AddServerModal/Paragraph/index.tsx
+++ src/components/modals/AddServerModal/Paragraph/index.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 interface ParagraphProps {
     title: string;
     text: string;
+    optional?: boolean;
 }
 
-export const Paragraph = ({title, text}: ParagraphProps) => {
+export const Paragraph = ({title, text, optional}: ParagraphProps) => {
   return (
     <Box>
-      <Heading fontSize={'15px'}>{title}</Heading>
+      <Heading fontSize={'15px'}>{optional && <Text as="span" color={'brand.100'}>*</Text>} {title}</Heading>
       <Text fontSize={'13px'} marginTop={2}>{text}</Text>
     </Box>
   );
--- src/components/modals/AddServerModal/index.tsx
+++ src/components/modals/AddServerModal/index.tsx
@@ -31,16 +31,16 @@ export const AddServerModal = ({isOpen, onClose}: AddServerModalProps) => {
             <Paragraph title={'Profesionalita a kvalitní reprezentace'} text={'Pozitivní recenze a kvalitní ohlasy na server dovedou tuto oblast skvěle popsat. Mimo jiné by se sám server měl prezentovat nekonfliktně a taktéž by měl být co nejvíce korektní'} />
             <Paragraph title={'Serverové komunitní platformy'} text={'Je velmi důležité, aby měl server alespoň jednu platformu, kam se mohou kromě Minecraftu lidé obrátit na podporu či hledat důležité informace. Jedná se například o TeamSpeak, Discord nebo obdobný komunikační nástroj'} />
             <Paragraph title={'Vlastní doména (př.: .cz, .sk, .eu)'} text={'Vlastní doménou se rozumí koupená a zaregistrovaná doména buď u českých nebo zahraničních registrátorů'} />
-            <Paragraph title={'* Denní průměr okolo 40 hráčů'} text={'40 hráčů pravidelně je hranice, která už není jednoduchá získat pouhým pozváním lidí ze svého streamu, většinou poukazuje na kvalitu serveru.'} />
-            <Paragraph title={'* Sociální sítě (př.: Instagram, Twitter, Facebook)'} text={'Server je aktivní na některé sociální síti. Sdílí pravidelně příspěvky a jejich prostřednictvím ohlašuje a sděluje nové informace nebo kterékoliv jiné serverové záležitosti'} />
-            <Paragraph title={'* Webové stránky'} text={'Webové stránky serveru mohou obsahovat záznamy o změnách, novinky, nebo například samostatné fórum a seznam členů admin-týmu'} />
+            <Paragraph title={'Denní průměr okolo 40 hráčů'} text={'40 hráčů pravidelně je hranice, která už není jednoduchá získat pouhým pozváním lidí ze svého streamu, většinou poukazuje na kvalitu serveru.'} optional/>
+            <Paragraph title={'Sociální sítě (př.: Instagram, Twitter, Facebook)'} text={'Server je aktivní na některé sociální síti. Sdílí pravidelně příspěvky a jejich prostřednictvím ohlašuje a sděluje nové informace nebo kterékoliv jiné serverové záležitosti'} optional/>
+            <Paragraph title={'Webové stránky'} text={'Webové stránky serveru mohou obsahovat záznamy o změnách, novinky, nebo například samostatné fórum a seznam členů admin-týmu'} optional/>
           </Flex>
         </ModalBody>
         <ModalFooter flexDirection={'column'} gap={4}>
-          <Text fontSize={'12px'}>
-            Odstavce označené jako * nejsou povinné, avšak při rozhodování o přijetí serveru dodávají plusové body
+          <Text fontSize={'12px'} color={'gray.100'} borderLeft={'4px solid'} roundedLeft={'sm'} paddingLeft={2} borderLeftColor={'brand.100'}>
+            Odstavce označené jako <Text as="span" color={'brand.100'}>*</Text> nejsou povinné, avšak při rozhodování o přijetí serveru dodávají plusové body
           </Text>
-          <Flex justifyContent={'flex-end'} width={'100%'}>
+          <Flex justifyContent={'flex-end'} width={'100%'} marginTop={2}>
             <Button colorScheme='red' variant={'outline'} mr={3} onClick={onClose}>Zavřít</Button>
             <Button variant='solid' colorScheme={'red'} as={'a'} href={'https://docs.google.com/forms/d/e/1FAIpQLSf5Igw_euENs9LtnXFRO7bcUKfZRQapvJylUOEGCnZMDECGhg/viewform'}>Pokračovat</Button>
           </Flex>
